1a4045900bfec4afd468b00e3495514a562b511d
Rafael Gieschke <rafael@gieschke.de>
2024-07-10 10:55:26 +0200

    feat(vm): allow to specify ports

--- modules/vm-openstack/main.tf
+++ modules/vm-openstack/main.tf
@@ -3,7 +3,8 @@ variable "config" {
     type            = optional(string, "m1.medium")
     image           = optional(string, "Ubuntu 22.04")
     size            = optional(number, 50)
-    security_groups = optional(list(string), ["http(s)"])
+    security_groups = optional(list(string), [])
+    ports           = optional(list(string), [])
   })
   default = {}
 }
@@ -17,13 +18,30 @@ data "openstack_images_image_v2" "image" {
   most_recent = true
 }
 
+resource "openstack_networking_secgroup_v2" "secgroup" {
+  count = length(var.config.ports) > 0 ? 1 : 0
+  name  = "ports ${join(", ", var.config.ports)}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_1" {
+  for_each          = toset(var.config.ports)
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = try(split("/", each.value)[1], "tcp")
+  port_range_min    = tonumber(split("/", each.value)[0])
+  port_range_max    = tonumber(split("/", each.value)[0])
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup[0].id
+}
+
 resource "openstack_compute_instance_v2" "server" {
-  name            = var.name
-  flavor_name     = var.config.type
-  image_id        = var.config.size > 0 ? null : data.openstack_images_image_v2.image.id
-  security_groups = var.config.security_groups
-  user_data       = var.user_data
-  key_pair        = "default"
+  name        = var.name
+  flavor_name = var.config.type
+  image_id    = var.config.size > 0 ? null : data.openstack_images_image_v2.image.id
+  security_groups = length(var.config.security_groups) == 0 && length(var.config.ports) == 0 ? ["default"] : concat(
+  try([openstack_networking_secgroup_v2.secgroup[0].name], []), var.config.security_groups)
+  user_data = var.user_data
+  key_pair  = "default"
 
   network {
     name = "public"
